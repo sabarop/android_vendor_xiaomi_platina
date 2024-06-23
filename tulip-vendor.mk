9424b7771f025b99e40d561e60ff813118e62cf5
Michael Bestas <mkbestas@lineageos.org>
2024-06-24 00:32:53 +0300

    tulip: Add symlinks
    Change-Id: I6eed2ca2574dd2bbb60a72337f525c555628d88b
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- tulip-vendor.mk
+++ tulip-vendor.mk
@@ -1130,3 +1130,21 @@ PRODUCT_PACKAGES += \
     qcrilhook \
     qti-telephony-common \
     vendor.qti.gnss@3.0-service.xml
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
+    eglSubDriverAndroid_eglSubDriverAndroid_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libGLESv1_CM_adreno_libGLESv1_CM_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libQTapGLES_libQTapGLES_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libq3dtools_esx_libq3dtools_esx_symlink32 \
+    eglSubDriverAndroid_eglSubDriverAndroid_symlink64 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv1_CM_adreno_libGLESv1_CM_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libQTapGLES_libQTapGLES_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64 \
+    libq3dtools_esx_libq3dtools_esx_symlink64
